a216fd5bc315b8d27ca8d6f451061d1ee9a5d98a
Rizart Dokollari <r.dokollari@linkwi.se>
2016-10-12 14:45:23 +0300

    First attempt of soling prime numbers with Jeffrey's help.

--- series/code-katas-in-php/app/PrimeFactor.php
+++ series/code-katas-in-php/app/PrimeFactor.php
@@ -4,4 +4,18 @@ namespace App;
 
 class PrimeFactor
 {
+    public function generate($number)
+    {
+        $primes = [];
+
+        for ($candidate = 2; $number > 1; $candidate++) {
+
+            for (; $number % $candidate == 0; $number /= $candidate) {
+
+                $primes[] = $candidate;
+            }
+        }
+
+        return $primes;
+    }
 }
--- series/code-katas-in-php/tests/unit/PrimeFactorSpec.php
+++ series/code-katas-in-php/tests/unit/PrimeFactorSpec.php
@@ -4,12 +4,66 @@ namespace tests\unit\App;
 
 use App\PrimeFactor;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class PrimeFactorSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType(PrimeFactor::class);
+    }
+
+    function it_computes_the_prime_number_of_one()
+    {
+        $this->generate(1)->shouldReturn([]);
+    }
+
+    function it_computes_the_prime_number_of_two()
+    {
+        $this->generate(2)->shouldReturn([2]);
+    }
+
+    function it_computes_the_prime_number_of_three()
+    {
+        $this->generate(3)->shouldReturn([3]);
+    }
+
+    function it_computes_the_prime_number_of_four()
+    {
+        $this->generate(4)->shouldReturn([2, 2]);
+    }
+
+    function it_computes_the_prime_number_of_five()
+    {
+        $this->generate(5)->shouldReturn([5]);
+    }
+
+    function it_computes_the_prime_number_of_six()
+    {
+        $this->generate(6)->shouldReturn([2, 3]);
+    }
+
+    function it_computes_the_prime_number_of_nine()
+    {
+        $this->generate(9)->shouldReturn([3, 3]);
+    }
+
+    function it_computes_the_prime_number_of_ten()
+    {
+        $this->generate(10)->shouldReturn([2, 5]);
+    }
+
+    function it_computes_the_prime_number_of_twenty_five()
+    {
+        $this->generate(25)->shouldReturn([5, 5]);
+    }
+
+    function it_computes_the_prime_number_of_forty_nine()
+    {
+        $this->generate(49)->shouldReturn([7, 7]);
+    }
+
+    function it_computes_the_prime_number_of_hundred()
+    {
+        $this->generate(100)->shouldReturn([2, 2, 5, 5]);
     }
 }
