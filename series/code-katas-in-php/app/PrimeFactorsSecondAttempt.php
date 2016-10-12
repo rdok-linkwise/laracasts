ea35583d221c45ed3d3661c56896aba981280644
Rizart Dokollari <r.dokollari@linkwi.se>
2016-10-12 15:16:24 +0300

    Second attempt without help.

---
+++ series/code-katas-in-php/app/PrimeFactorsSecondAttempt.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class PrimeFactorsSecondAttempt
+{
+    public function generate($number)
+    {
+        $primes = [];
+
+        for ($candidate = 2; $number > 1; $candidate++) {
+            for (; $number % $candidate == 0; $number /= $candidate) {
+                $primes[] = $candidate;
+            }
+        }
+
+        return $primes;
+    }
+}
---
+++ series/code-katas-in-php/tests/unit/PrimeFactorsSecondAttemptSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace tests\unit\App;
+
+use App\PrimeFactorsSecondAttempt;
+use App\PrimeFactorsSendAttempt;
+use PhpSpec\ObjectBehavior;
+
+class PrimeFactorsSecondAttemptSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(PrimeFactorsSecondAttempt::class);
+    }
+
+    function it_computes_prime_number_for_one()
+    {
+        $this->generate(1)->shouldReturn([]);
+    }
+
+    function it_computes_prime_number_for_two()
+    {
+        $this->generate(2)->shouldReturn([2]);
+    }
+
+    function it_computes_prime_number_for_three()
+    {
+        $this->generate(3)->shouldReturn([3]);
+    }
+
+    function it_computes_prime_number_for_four()
+    {
+        $this->generate(4)->shouldReturn([2, 2]);
+    }
+
+    function it_computes_prime_number_for_five()
+    {
+        $this->generate(5)->shouldReturn([5]);
+    }
+
+    function it_computes_prime_number_for_six()
+    {
+        $this->generate(6)->shouldReturn([2, 3]);
+    }
+
+    function it_computes_prime_number_for_eight()
+    {
+        $this->generate(8)->shouldReturn([2, 2, 2]);
+    }
+
+    function it_computes_prime_number_for_nine()
+    {
+        $this->generate(9)->shouldReturn([3, 3]);
+    }
+}
